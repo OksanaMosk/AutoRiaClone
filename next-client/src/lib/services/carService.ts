613af2d5a80a4c8afd7e9e1fc0bbbd55b067eb65
OksanaMosk <oks-m3@ukr.net>
2025-11-14 23:06:51 +0200

    next

--- next-client/src/lib/services/carService.ts
+++ next-client/src/lib/services/carService.ts
@@ -1,64 +1,63 @@
 import {urls, urls as paths} from "../constants/urls";
-import { apiService } from "./apiService";
-import { ICar, ICarPhoto } from "@/models/ICar";
+import {apiService} from "./apiService";
+import {ICar, ICarPhoto} from "@/models/ICar";
 
 const carService = {
-  action: (id: string) => `${paths.cars}/${id}/`,
+    action: (id: string) => `${paths.cars}/${id}/`,
 
-  getAll() {
-    return apiService.get(urls.cars.list);
-  },
+    getAll() {
+        return apiService.get(urls.cars.list);
+    },
 
-  get(id: string) {
-    return apiService.get<ICar>(urls.cars.action(id));
-  },
+    get(id: string) {
+        return apiService.get<ICar>(urls.cars.action(id));
+    },
 
-  create(data: ICar) {
-    return apiService.post<ICar>(urls.cars.create, data);
-  },
+    create(data: ICar) {
+        return apiService.post<ICar>(urls.cars.create, data);
+    },
 
- update(id: string, data: Partial<ICar>) {
-  return apiService.put<ICar>(urls.cars.action(id), data);
-},
+    update(id: string, data: Partial<ICar>) {
+        return apiService.put<ICar>(urls.cars.action(id), data);
+    },
 
-  delete(id: string) {
-    return apiService.delete(urls.cars.action(id));
-  },
+    delete(id: string) {
+        return apiService.delete(urls.cars.action(id));
+    },
 
+    addPhoto(carId: string, photoData: ICarPhoto[]) {
+        if (photoData.length > 5) {
+            throw new Error("You can only upload up to 5 photos.");
+        }
+        return apiService.post(urls.cars.photos(carId), photoData);
+    },
 
-  addPhoto(carId: string, photoData: ICarPhoto[]) {
-    if (photoData.length > 5) {
-      throw new Error("You can only upload up to 5 photos.");
-    }
-    return apiService.post(urls.cars.photos(carId), photoData);
-  },
-
-  deletePhoto(photoId: string) {
-   return apiService.delete(urls.cars.deletePhoto(photoId));
-  },
+    deletePhoto(photoId: string) {
+        return apiService.delete(urls.cars.deletePhoto(photoId));
+    },
 
     getExchangeRates() {
-    return apiService.get(urls.cars.exchangeRates);
-  },
+        return apiService.get(urls.cars.exchangeRates);
+    },
 
-  getStats(carId: string) {
-    return apiService.get(urls.cars.stats(carId));
-  },
+    getStats(carId: string) {
+        return apiService.get(urls.cars.stats(carId));
+    },
 
-  getAveragePriceByRegion() {
-    return apiService.get(urls.cars.averagePriceRegion);
-  },
+    getAveragePriceByRegion() {
+        return apiService.get(urls.cars.averagePriceRegion);
+    },
 
-  getAveragePriceByCountry() {
-    return apiService.get(urls.cars.averagePriceCountry);
-  },
+    getAveragePriceByCountry() {
+        return apiService.get(urls.cars.averagePriceCountry);
+    },
 
     getConstants() {
-    return apiService.get(urls.cars.constants);
-  },
-      getUserCars(userId: string) {
-    return apiService.get<ICar[]>(urls.cars.userCars(userId));
-  },
+        return apiService.get(urls.cars.constants);
+    },
+    getUserCars(userId: string) {
+        return apiService.get<ICar[]>(urls.cars.userCars(userId));
+    },
 };
 
-export { carService };
+export {carService};
